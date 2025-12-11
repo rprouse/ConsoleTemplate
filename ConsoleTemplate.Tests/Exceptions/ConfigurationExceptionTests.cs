638dd8eab7b5874b0863cc8a04bc40f321977419
Rob Prouse <rob.prouse@aspiresoftware.com>
2025-12-11 10:00:38 -0700

    Add unit tests

---
+++ ConsoleTemplate.Tests/Exceptions/ConfigurationExceptionTests.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleTemplate.Exceptions;
+
+namespace ConsoleTemplate.Tests.Exceptions;
+
+public class ConfigurationExceptionTests
+{
+    [Test]
+    public void Constructor_SetsKeyProperty()
+    {
+        // Arrange
+        var key = "MY_KEY";
+
+        // Act
+        var ex = new ConfigurationException(key);
+
+        // Assert
+        ex.Key.ShouldBe(key);
+    }
+
+    [Test]
+    public void Constructor_SetsMessage()
+    {
+        // Arrange
+        var key = "TEMP_FOLDER";
+
+        // Act
+        var ex = new ConfigurationException(key);
+
+        // Assert
+        ex.Message.ShouldBe($"Configuration for {key} is not set");
+    }
+
+    [Test]
+    public void Constructor_AllowsNullKey()
+    {
+        // Act
+        var ex = new ConfigurationException(null!);
+
+        // Assert
+        ex.Key.ShouldBeNull();
+        ex.Message.ShouldBe("Configuration for  is not set");
+    }
+}
---
+++ ConsoleTemplate.Tests/Extensions/ConfigurationExtensionTests.cs
@@ -0,0 +1,60 @@
+﻿using ConsoleTemplate.Exceptions;
+using ConsoleTemplate.Extensions;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleTemplate.Tests.Extensions;
+
+public class ConfigurationExtensionTests
+{
+    [Test]
+    public void GetRequiredValue_ReturnsValue_WhenKeyExists()
+    {
+        // Arrange
+        var values = new Dictionary<string, string?>
+        {
+            ["TEMP_FOLDER"] = "/tmp"
+        };
+
+        var config = new ConfigurationBuilder()
+            .AddInMemoryCollection(values!)
+            .Build();
+
+        // Act
+        var result = config.GetRequiredValue("TEMP_FOLDER");
+
+        // Assert
+        result.ShouldBe("/tmp");
+    }
+
+    [Test]
+    public void GetRequiredValue_ThrowsConfigurationException_WhenKeyMissing()
+    {
+        // Arrange
+        var config = new ConfigurationBuilder().Build();
+        var key = "MISSING_KEY";
+
+        // Act & Assert
+        var ex = Should.Throw<ConfigurationException>(() => config.GetRequiredValue(key));
+        ex.Key.ShouldBe(key);
+    }
+
+    [Test]
+    public void GetRequiredValue_AllowsEmptyStringValue()
+    {
+        // Arrange
+        var values = new Dictionary<string, string?>
+        {
+            ["TEMP_FOLDER"] = string.Empty
+        };
+
+        var config = new ConfigurationBuilder()
+            .AddInMemoryCollection(values!)
+            .Build();
+
+        // Act
+        var result = config.GetRequiredValue("TEMP_FOLDER");
+
+        // Assert
+        result.ShouldBe(string.Empty);
+    }
+}
